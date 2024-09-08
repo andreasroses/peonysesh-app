c947d9b8734d150825f8001d184e158b3353a55b
Andrea Mendez <andreacelestem11@gmail.com>
2024-09-07 21:49:55 -0500

    break needed sigh

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
         <ThemeProvider>
           <ClientThemeWrapper>
             <Navbar />
-            {/* <br></br> */}
+            <br></br>
             <div className="flex-grow">
               {children}
             </div>
